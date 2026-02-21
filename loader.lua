95cd90797117fe74fca569af9b6e646ea438c626
ADMZYL <legendarykill190@gmail.com>
2026-02-21 11:16:30 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/eIysia-dev/release/refs/heads/main/open-src.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/4a873606f2053bf3067ad6da43b5baee.lua"))()
 loadstring(game:HttpGet("https://pastefy.app/je3gk5AF/raw"))()
